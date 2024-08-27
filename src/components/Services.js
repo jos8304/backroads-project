2548e17417c0284540ffc6425927985538d94dac
Jason <jos8304@hotmail.com>
2024-08-27 16:53:56 -0400

    Ract-website

---
+++ src/components/Services.js
@@ -0,0 +1,26 @@
+import Title from "./Title";
+import { services } from "../data";
+
+const Services = () => {
+  return (
+    <section className="section services" id="services">
+      <Title title="our" subTitle="services" />
+      <div className="section-center services-center">
+        {services.map((link) => {
+          return (
+            <article className="service">
+              <span className="service-icon">
+                <i className={link.icon}></i>
+              </span>
+              <div className="service-info">
+                <h4 className="service-title">{link.title}</h4>
+                <p className="service-text">{link.text}</p>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+export default Services;
---
+++ src/data.js
@@ -0,0 +1,36 @@
+export const pageLinks = [
+  { id: 1, href: "#home", text: "home" },
+  { id: 2, href: "#about", text: "about" },
+  { id: 3, href: "#services", text: "services" },
+  { id: 4, href: "#tours", text: "tours" },
+];
+
+export const socialLinks = [
+  { id: 1, href: "https://github.com/jos8304", className: "fab fa-github" },
+  {
+    id: 2,
+    href: "https://www.linkedin.com/in/jason-yoo-3575b1266/",
+    className: "fab fa-linkedin",
+  },
+];
+
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia.",
+  },
+  {
+    id: 2,
+    icon: "fas fa-tree fa-fw",
+    title: "endless hiking",
+    text: "orem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia.",
+  },
+  {
+    id: 3,
+    icon: "fas fa-socks fa-fw",
+    title: "amazing comfort",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia.",
+  },
+];
