f26cc8e67df89137712ad8eb181e5cc390566e98
Jason <jos8304@hotmail.com>
2024-08-27 17:07:42 -0400

    add_tours

--- src/data.js
+++ src/data.js
@@ -1,3 +1,8 @@
+import tour1 from "./images/tour-1.jpeg";
+import tour2 from "./images/tour-2.jpeg";
+import tour3 from "./images/tour-3.jpeg";
+import tour4 from "./images/tour-4.jpeg";
+
 export const pageLinks = [
   { id: 1, href: "#home", text: "home" },
   { id: 2, href: "#about", text: "about" },
@@ -32,5 +37,56 @@ export const services = [
     icon: "fas fa-socks fa-fw",
     title: "amazing comfort",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia.",
+  },
+];
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1,
+    date: "october 1th, 2020",
+    title: "best of java",
+    info: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
+              vitae tempore voluptatum maxime reprehenderit eum quod
+              exercitationem fugit, qui corporis.`,
+    location: "indonesia",
+    duration: 6,
+    price: "from $1400",
+  },
+  {
+    id: 2,
+    image: tour2,
+    date: "october 1th, 2020",
+    title: "best of java",
+    info: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
+              vitae tempore voluptatum maxime reprehenderit eum quod
+              exercitationem fugit, qui corporis.`,
+    location: "indonesia",
+    duration: 6,
+    price: "from $1400",
+  },
+  {
+    id: 3,
+    image: tour3,
+    date: "october 1th, 2020",
+    title: "best of java",
+    info: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
+              vitae tempore voluptatum maxime reprehenderit eum quod
+              exercitationem fugit, qui corporis.`,
+    location: "indonesia",
+    duration: 6,
+    price: "from $1400",
+  },
+  {
+    id: 4,
+    image: tour4,
+    date: "october 1th, 2020",
+    title: "best of java",
+    info: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque
+              vitae tempore voluptatum maxime reprehenderit eum quod
+              exercitationem fugit, qui corporis.`,
+    location: "indonesia",
+    duration: 6,
+    price: "from $1400",
   },
 ];
